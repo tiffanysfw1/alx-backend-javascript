98d6659c57653174eb62eaaff7630f12a3957bfe
tiffanysfw1 <tiffyjemohappy19@gmail.com>
2023-11-09 12:21:40 -0500

    tasks

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,16 +1,12 @@
 /**
- * Retrieves ids from a list of students.
- * @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- * }[]} students - The list of students.
+ * Retrieves a list of students.
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns
+ * @returns {{id: Number, firstName: String, location: String}[]}
  */
-export default function getListStudentIds(students) {
-  if (students instanceof Array) {
-    return students.map((student) => student.id);
-  }
-  return [];
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
